c8b3a711869474d15fc2a3b932fb990f3f9c3feb
Tho Lee <tho.le@nfq.com>
2025-10-01 21:18:37 +0700

    Integrate `MemoApp` and `ThemeContext` components into root render in '02 Thinking in React' app

--- 02 Thinking in React/src/index.js
+++ 02 Thinking in React/src/index.js
@@ -3,9 +3,16 @@ import {createRoot} from 'react-dom/client';
 
 import App from './App';
 import Counter from "./Counter";
+import ThemeContext from "./Context";
+import MemoApp from "./Memo";
 
 const root = createRoot(document.getElementById('root'));
 root.render(<StrictMode>
-    <div className="App"><App/></div>
+    <div className="MemoApp"><MemoApp/></div>
+    <hr />
     <div className="Counter"><Counter/></div>
+    <hr />
+    <div className="ThemeContext"><ThemeContext /></div>
+    <hr />
+    <div className="App"><App/></div>
 </StrictMode>)
